8f94042d80a8df7f729ac350e9fb49356a92bf11
rittik sheikh <rittik11500@gamil.com>
2023-05-14 21:48:36 +0600

    Navbar done currently

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -1,24 +1,25 @@
 import React, { useState } from 'react';
+import logo from '../../../assets/images/ebook.png';
+import { HiBars3BottomRight } from "react-icons/hi2";
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-    const [navbarOpen, setNavbarOpen] = useState(false);
-    return (
-        <div>
-                  <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-pink-500 mb-3">
+  const [navbarOpen, setNavbarOpen] = useState(false);
+  return (
+    <div>
+      <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-blue-500 mb-3">
         <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
           <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
-            <a
-              className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-              href="#pablo"
-            >
-              pink Tailwind Starter Kit
-            </a>
+              <Link to='/home'>
+              <img className='w-[50px] h-[45px] mr-1 inline' src={logo} alt="" />
+              </Link>
+                <h4 className="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap text-white">ProTech.edu</h4>
             <button
               className="text-white cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
               type="button"
               onClick={() => setNavbarOpen(!navbarOpen)}
             >
-              <i className="fas fa-bars"></i>
+             <HiBars3BottomRight></HiBars3BottomRight>
             </button>
           </div>
           <div
@@ -28,37 +29,48 @@ const Header = () => {
             }
             id="example-navbar-danger"
           >
-            <ul className="flex flex-col lg:flex-row list-none lg:ml-auto">
+            <ul className="flex flex-col mt-4 lg:mt-0  lg:flex-row list-none lg:ml-auto">
               <li className="nav-item">
-                <a
+                <Link to='/courses'
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
                 >
-                  <i className="fab fa-facebook-square text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Share</span>
-                </a>
+                  <i className="text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Courses</span>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
+                <Link to='/blog'
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
                 >
-                  <i className="fab fa-twitter text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Tweet</span>
-                </a>
+                  <i className="text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Blog</span>
+                </Link>
               </li>
               <li className="nav-item">
-                <a
+                <Link to='/faq'
                   className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                  href="#pablo"
                 >
-                  <i className="fab fa-pinterest text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Pin</span>
-                </a>
+                  <i className="text-lg leading-lg text-white opacity-75"></i><span className="ml-2">FAQ</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/profile'
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                >
+                  <i className="text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Profile</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to='/toggleTheme'
+                  className="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
+                >
+                  <i className="text-lg leading-lg text-white opacity-75"></i><span className="ml-2">Toggle Theme</span>
+                </Link>
               </li>
             </ul>
           </div>
         </div>
       </nav>
-        </div>
-    );
+    </div>
+  );
 };
 
 export default Header;
